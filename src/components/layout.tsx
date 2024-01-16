2fc668546982f31268f1e10a849bb40e4adc1603
Kyuyeon Kim <pcaso5@naver.com>
2024-01-16 10:28:39 +0900

    [KKY] 레이아웃 변경 및 내용 수정 반영

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -5,28 +5,78 @@ import Container from "./container";
 const Layout = ({ children, ...props }: ComponentPropsWithoutRef<"div">) => {
   return (
     <div className="flex flex-col min-h-screen" {...props}>
-      <header className="sticky left-0 top-0 z-40 w-full px-5 bg-white bg-opacity-70">
-        <Container className="flex gap-2 items-center py-5">
-          <Link href="/">
-            <img src="/max_logo.png" alt="max-logo" className="w-10" />
-          </Link>
-          <ul className="flex gap-2">
+      <div className="drawer drawer-end">
+        <input id="drawer-toggle" type="checkbox" className="drawer-toggle" />
+        <div className="drawer-content">
+          <header className="sticky left-0 top-0 z-40 w-full px-5 bg-white bg-opacity-70">
+            <Container className="flex gap-10 items-center py-5">
+              <Link className="shrink-0 flex items-center gap-1" href="/">
+                <img src="/max_logo.png" alt="max-logo" className="w-10" />
+                <h1 className="text-2xl font-black text-orange-600 shrink-0">맥스팀</h1>
+              </Link>
+              <ul className="gap-10 hidden sm:flex">
+                <li>
+                  <Link className="text-xl font-bold link-hover hover:text-orange-600" href="/team">
+                    팀소개
+                  </Link>
+                </li>
+                <li>
+                  <Link className="text-xl font-bold link-hover hover:text-orange-600" href="/meet">
+                    문의방법
+                  </Link>
+                </li>
+                <li>
+                  <Link className="text-xl font-bold link-hover hover:text-orange-600" href="/kotch">
+                    강사진
+                  </Link>
+                </li>
+                <li>
+                  <Link className="text-xl font-bold link-hover hover:text-orange-600" href="/price">
+                    가격표
+                  </Link>
+                </li>
+              </ul>
+              <label className="ml-auto sm:hidden" htmlFor="drawer-toggle">
+                <svg stroke="currentColor" fill="currentColor" stroke-width="0" viewBox="0 0 512 512" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg">
+                  <path d="M32 96v64h448V96H32zm0 128v64h448v-64H32zm0 128v64h448v-64H32z"></path>
+                </svg>
+              </label>
+            </Container>
+          </header>
+          {children}
+        </div>
+        <nav className="drawer-side z-50">
+          <label className="drawer-overlay" htmlFor="drawer-toggle" />
+          <ul className="flex flex-col items-center gap-5 bg-white h-full px-10 py-10">
             <li>
-              <Link href="/team">팀소개</Link>
+              <Link className="shrink-0 flex flex-col items-center gap-1" href="/">
+                <img src="/max_logo.png" alt="max-logo" className="w-10" />
+                <h1 className="font-black text-orange-600 shrink-0">맥스팀</h1>
+              </Link>
             </li>
             <li>
-              <Link href="/meet">문의방법</Link>
+              <Link className="text-xl font-bold link-hover hover:text-orange-600" href="/team">
+                팀소개
+              </Link>
             </li>
             <li>
-              <Link href="/kotch">강사진</Link>
+              <Link className="text-xl font-bold link-hover hover:text-orange-600" href="/meet">
+                문의방법
+              </Link>
             </li>
             <li>
-              <Link href="/price">가격표</Link>
+              <Link className="text-xl font-bold link-hover hover:text-orange-600" href="/kotch">
+                강사진
+              </Link>
+            </li>
+            <li>
+              <Link className="text-xl font-bold link-hover hover:text-orange-600" href="/price">
+                가격표
+              </Link>
             </li>
           </ul>
-        </Container>
-      </header>
-      {children}
+        </nav>
+      </div>
       <a href="https://open.kakao.com/o/sErrvW3f" target="_blank" className="fixed right-5 bottom-5">
         <span className="tooltip tooltip-left tooltip-warning" data-tip="카카오 오픈채팅 바로가기">
           <img src="open_kakao.png" className="w-10 animate-bounce" />
--- src/pages/team/index.tsx
+++ src/pages/team/index.tsx
@@ -12,16 +12,11 @@ const TeamPage = () => {
           <img src="team_banner.png" alt="팀 배너 이미지" />
         </figure>
         <div className="flex flex-col gap-2 max-w-[40rem] mx-auto py-4">
-          <p>저희 맥스팀은 카카오톡 , 텔레그램으로만 문의를 받고 있습니다.</p>
-          <p className="badge badge-lg font-black bg-orange-600 text-white">카카오톡 ID: MAXTEAM</p>
-          <p className="badge badge-lg font-black bg-orange-600 text-white">텔레그램 ID: MAXTEAMCOM</p>
-          <p>이 외에는 전부 사칭 또는 사기이니 주의해주시기 바랍니다.</p>
-          <p>오픈채팅으로 익명상담을 원하시는 고객님께서는 아래 이미지 클릭해주시면 연결가능합니다.</p>
-        </div>
-        <div className="mx-auto w-max">
-          <a className="btn bg-orange-600 text-white hover:text-orange-600 hover:bg-white hover:border-orange-600" target="_blank" href="https://open.kakao.com/o/sErrvW3f">
-            카카오톡 오픈채팅
-          </a>
+          <p>안녕하세요 오버워치2 전문 강의 팀 맥스팀입니다.</p>
+          <p>저희는 오버워치 승당제 강의, 티어제 강의 , 배치고사 강의, 듀오 강의, 승패무관 강의, 1:1 강의 등 오버워치 관련 서비스를 제공하고 있습니다.</p>
+          <p>저희 팀은 모두 최소 전 프로, 1페이지 강사님들로 구성되어 있으며, 최선을 다해 수강생분들의 목표 티어를 위해 노력할 것을 약속합니다.</p>
+          <p>원하신다면 모든 강사님들이 방송으로 강의를 진행하기 때문에 실시간으로 수강생분들이 강의를 제공받으실 수 있습니다.</p>
+          <p>타 업체에 비해 합리적인 가격으로 진행하고 있으니 많은 문의 부탁드립니다</p>
         </div>
       </Container>
     </main>
