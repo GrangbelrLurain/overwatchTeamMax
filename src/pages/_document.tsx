a30be0881cebd417ea7bc8440487f1303b6c3228
Lurain Grangbelr <pcaso5@naver.com>
2024-01-17 12:26:08 +0900

    [KKY] 네이버 서치어드바이저 등록

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -5,6 +5,10 @@ export default function Document() {
     <Html lang="en">
       <Head>
         <link rel="shortcut icon" href="max_logo.png" />
+        <meta
+          name="naver-site-verification"
+          content="523d5596e009b7e5fce9c2038915e67947c45ed8"
+        />
       </Head>
       <body data-theme="light">
         <Main />
