915e4555672fb7b82b40362c43fec29e64ad0c4c
Lurain Grangbelr <pcaso5@naver.com>
2024-01-17 12:56:10 +0900

    [KKY] 구글 소유권 확인

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -9,6 +9,10 @@ export default function Document() {
           name="naver-site-verification"
           content="523d5596e009b7e5fce9c2038915e67947c45ed8"
         />
+        <meta
+          name="google-site-verification"
+          content="2Y9LpKRMZs2DP2S4h1nJ-WghojYTxoAVuMRx3el3glo"
+        />
       </Head>
       <body data-theme="light">
         <Main />
