addd7a57fa8c83319f9aacd8e97f278ef9492646
Kyu-Yeon Kim <lurain003@gmail.com>
2024-04-13 22:32:20 +0900

    _document.tsx 업데이트

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -5,7 +5,7 @@ export default function Document() {
     <Html lang="en">
       <Head>
         <link rel="shortcut icon" href="max_logo.png" />
-        <meta name="keywords" content="오버워치2, 첫구매10%할인" />
+        <meta name="keywords" content="오버워치2" />
         <meta name="naver-site-verification" content="523d5596e009b7e5fce9c2038915e67947c45ed8" />
         <meta name="google-site-verification" content="2Y9LpKRMZs2DP2S4h1nJ-WghojYTxoAVuMRx3el3glo" />
       </Head>
